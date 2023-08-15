f3cfa21669d0203750280ccc4d9096ed81fc541b
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-16 00:00:01 +0530

    first commit

---
+++ 1. TwoSum/Solution.java
@@ -0,0 +1,38 @@
+
+//brute force
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int s = nums.length;
+
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < s; j++) {
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
+
+// hash map
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int s = nums.length;
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < s; i++) {
+            int comp = target - nums[i];
+            if (numMap.containsKey(comp)) {
+                return new int[] { numMap.get(comp), i };
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}

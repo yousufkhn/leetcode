e527220bf9ed11db9a81dcacfdfb6c8a04917929
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-18 10:42:30 +0530

    solved valid parantheses

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,25 @@
+//using stack
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else if(c==')' && !stack.isEmpty() && stack.peek()=='('){
+                stack.pop();
+            }
+            else if(c==']' && !stack.isEmpty() && stack.peek()=='['){
+                stack.pop();
+            }
+            else if(c=='}' && !stack.isEmpty() && stack.peek()=='{'){
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}

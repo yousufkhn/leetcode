4bca37ac3e2af3c462ccfaba55142f6c6355e608
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-21 15:47:20 +0530

    removed duplicates from sorted array - done

---
+++ 26. Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i=1;i<nums.length;i++){
+            if (nums[i] !=nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+
+    }
+}

5a11d4dd590d300561484cff50782bfd9cf89524
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-11-04 12:38:35 +0530

    lst moment before ant falls - done

---
+++ 1503. Last Moment Before All Ants Fall Out of a Plank/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int maxDistance;
+        int rightMin;
+        if (right.length==0){
+            rightMin=n;
+        }
+        else{
+            rightMin=right[0];
+        }
+        
+        int leftMax=0;
+        for (int i = 0; i < right.length; i++) {
+        if (right[i] < rightMin) {
+            rightMin = right[i];
+        }
+        }
+        rightMin=n-rightMin;
+        for (int i = 0; i < left.length; i++) {
+        if ( left[i] >  leftMax) {
+             leftMax =  left[i];
+        }
+    }
+        maxDistance= Math.max(rightMin, leftMax);
+        return maxDistance;
+    
+}} Solution {
+    
+}

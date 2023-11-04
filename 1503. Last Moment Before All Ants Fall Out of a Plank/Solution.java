a1660eb31fb345d0b02e2f192ba78a61df3e1a31
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-11-04 16:37:03 +0530

    reverse linked list - done

--- 1503. Last Moment Before All Ants Fall Out of a Plank/Solution.java
+++ 1503. Last Moment Before All Ants Fall Out of a Plank/Solution.java
@@ -1,29 +1,27 @@
-public class Solution {
+class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
         int maxDistance;
         int rightMin;
-        if (right.length==0){
-            rightMin=n;
+        if (right.length == 0) {
+            rightMin = n;
+        } else {
+            rightMin = right[0];
         }
-        else{
-            rightMin=right[0];
-        }
-        
-        int leftMax=0;
+
+        int leftMax = 0;
         for (int i = 0; i < right.length; i++) {
-        if (right[i] < rightMin) {
-            rightMin = right[i];
+            if (right[i] < rightMin) {
+                rightMin = right[i];
+            }
         }
-        }
-        rightMin=n-rightMin;
+        rightMin = n - rightMin;
         for (int i = 0; i < left.length; i++) {
-        if ( left[i] >  leftMax) {
-             leftMax =  left[i];
+            if (left[i] > leftMax) {
+                leftMax = left[i];
+            }
         }
-    }
-        maxDistance= Math.max(rightMin, leftMax);
+        maxDistance = Math.max(rightMin, leftMax);
         return maxDistance;
-    
-}} Solution {
-    
+
+    }
 }
